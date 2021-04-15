ac6d05233d8aa418a6711c16390a1ddace092572
Gabriel <dmenezes.gabriel@gmail.com>
2021-04-15 15:41:50 -0300

    Add game class

--- cli/main.js
+++ cli/main.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import User from "./src/user.js";
+import Game from "./src/game.js";
 
 dotenv.config();
 
@@ -14,3 +15,7 @@ console.log(user.userId);
 console.log("get characters");
 await user.getCharacters();
 console.log(user.characters);
+
+let game = new Game(user.sessionCookie, user.userId);
+await game.getServers();
+console.log(game.servers);
---
+++ cli/src/game.js
@@ -0,0 +1,29 @@
+import api from "./api.js";
+
+export default class Game {
+  constructor(sessionCookie, userId) {
+    this.sessionCookie = sessionCookie;
+    this.userId = userId;
+    this.servers = {};
+  }
+
+  async getServers() {
+    if (!this.userId) return Promise.reject("You must login first.");
+    console.info(`Getting Servers`);
+    const serversResponse = await api.post(
+      "servers_and_characters",
+      "method=servers_and_characters&arguments={}",
+      { headers: { cookie: `auth=${this.sessionCookie}-${this.userId}` } }
+    );
+    if (serversResponse.status == 200) {
+      for (const characterData of serversResponse.data[0].servers) {
+        this.servers[characterData.name] = characterData;
+      }
+      return Promise.resolve(true);
+    } else {
+      console.error(serversResponse);
+    }
+
+    return Promisse.reject("Error fetching servers");
+  }
+}
