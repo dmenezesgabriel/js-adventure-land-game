f1ce4d087862bfe69b06325bbfe6aa6f2bd9f32e
Gabriel <dmenezes.gabriel@gmail.com>
2021-04-15 17:27:43 -0300

    Add rotatin file logger

--- cli/src/logger.js
+++ cli/src/logger.js
@@ -6,13 +6,13 @@ const logger = winston.createLogger({
     winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     winston.format.align(),
     winston.format.printf(
-      (info) => `[ ${info.timestamp} ]  ${info.level}: ${info.message}`
+      (info) => `${info.timestamp} - [${info.level}]: ${info.message}`
     )
   ),
   level: "info",
   transports: [
-    // new winston.transports.File({ filename: "error.log", level: "error" }),
-    // new winston.transports.File({ filename: "all.log" }),
+    // new winston.transports.File({ filename: "error.log", level: "error", maxSize: 500 }),
+    // new winston.transports.File({ filename: "all.log", maxSize: 500 }),
     new winston.transports.Console(),
   ],
 });
