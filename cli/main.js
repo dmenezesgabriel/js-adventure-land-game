ad5b15607019f27826f41a0e5d80aa99c4489798
Gabriel <dmenezes.gabriel@gmail.com>
2021-04-16 11:47:09 -0300

    Add a new hope to sockets

--- cli/main.js
+++ cli/main.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import User from "./src/user.js";
 import Game from "./src/game.js";
 import logger from "./src/logger.js";
-import Server from "./src/server.js";
 import Character from "./src/character.js";
 
 logger.info("Hello Adventurer");
@@ -24,19 +23,15 @@ logger.info("Getting characters");
 await game.getServers();
 // console.log(game.servers);
 
-let currentServer = game.servers["III"];
-
-logger.info("Connecting to server");
-const server = new Server(currentServer);
-server.connect();
-// let character = new Character(
-//   user.characters["Pumafang"]["id"],
-//   user.userId,
-//   user.sessionCookie,
-//   currentServer.socket
-// );
-// logger.info("Connecting character");
-// character.connect();
+logger.info("Connecting character");
+let character = new Character(
+  game.servers["III"],
+  user.characters["Pumafang"]["id"],
+  user.userId,
+  user.sessionCookie
+);
+logger.info("Connecting character");
+character.connect();
 
 // logger.info("Disconnecting from server");
-server.disconnect();
+// server.disconnect();
--- cli/src/character.js
+++ cli/src/character.js
@@ -1,28 +1,68 @@
-import Server from "./server.js";
-
+import io from "socket.io-client";
 export default class Character {
-  constructor(characterId, userId, sessionCookie, socket) {
+  constructor(serverData, characterId, userId, sessionCookie) {
+    this.serverRegion = serverData.region;
+    this.serverName = serverData.name;
+    this.serverAddr = serverData.addr;
+    this.serverPort = serverData.port;
     this.characterId = characterId;
     this.userId = userId;
     this.sessionCookie = sessionCookie;
-    this.socket = socket;
+    this.socket = {};
   }
-  // Connect
-  // Disconnect
+
   async connect() {
-    this.socket.on("welcome", () => {
-      this.socket.emit("auth", {
-        auth: this.sessionCookie,
-        character: this.characterId,
-        height: 1080,
-        no_graphics: "True",
-        no_html: "1",
-        passphrase: "",
-        scale: 2,
-        user: this.userId,
-        width: 1920,
-      });
+    console.debug(`Connecting to ${this.serverRegion}:${this.serverName}...`);
+    console.debug(`Connecting to ${this.serverAddr}:${this.serverPort}...`);
+    this.socket = io(`wss://${this.serverAddr}:${this.serverPort}`, {
+      secure: true,
+      transports: ["websocket"],
     });
-    socket.open();
+
+    let socket = this.socket.connect();
+    let original_onevent = socket.onevent;
+
+    socket.onevent = function (packet) {
+      console.log("INCOMING", JSON.stringify(arguments) + " " + new Date());
+      original_onevent.apply(socket, arguments);
+    };
+
+    socket.on("connect", function () {
+      console.log("connected");
+    });
+
+    socket.on("welcome", (data) => {
+      console.log("welcome " + data);
+    });
+
+    socket.emit("auth", {
+      user: this.userId,
+      character: this.characterId,
+      code_slot: this.characterId,
+      auth: this.sessionCookie,
+      width: 768,
+      height: 1366,
+      no_html: "",
+      passphrase: "",
+      scale: "2",
+      no_graphics: "",
+    });
+
+    socket.on("auth", (data) => {
+      console.log("auth" + data);
+    });
+
+    socket.on("server_info", (data) => console.log("serv_info " + data));
+
+    socket.connect();
+
+    console.log("done");
+  }
+
+  async disconnect() {
+    console.debug(
+      `Disconnecting from ${this.serverRegion}:${this.serverName}...`
+    );
+    this.socket.close();
   }
 }
