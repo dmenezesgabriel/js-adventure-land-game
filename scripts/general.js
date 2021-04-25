311aab0ea2a2577da76d91244ce1964fbc616f51
Gabriel <dmenezes.gabriel@gmail.com>
2021-04-24 21:24:31 -0300

    Update general script to spadar's and add console logs

--- scripts/general.js
+++ scripts/general.js
@@ -1,77 +1,246 @@
-var reviving = true;
-var targetMonster = "goo";
-var fighting = true;
+//Put monsters you want to kill in here
+//If your character has no target, it will travel to a spawn of the first monster in the list below.
+var monster_targets = ["crab"];
 
-if (character.rip) {
-  setTimeout(function () {
-    respawn();
-    setTimeout(function () {
-      smart_move(targetMonster, function () {
-        fighting = true;
-      });
-    }, 1000);
-  }, 12000);
-} else {
-  smart_move(targetMonster, function () {
-    fighting = true;
-  });
+var state = "farm";
+
+var min_potions = 50; //The number of potions at which to do a resupply run.
+var purchase_amount = 50; //How many potions to buy at once.
+var potion_types = ["hpot0", "mpot0"]; //The types of potions to keep supplied.
+var should_heal_hp = 0.5;
+var should_heal_mp = 0.5;
+
+// Log to console character name following by message
+function logCharacter(message) {
+  console.log(`${character.name} - ${message}`);
+  game_log(`${character.name} - ${message}`);
 }
 
+//Movement And Attacking
 setInterval(function () {
-  if (!fighting) {
-    return;
-  }
+  //Determine what state we should be in.
+  state_controller();
 
-  if (character.rip && !reviving) {
-    reviving = true;
-    fighting = false;
-    var position = {
-      x: character.real_x,
-      y: character.real_y,
-      map: character.map,
-    };
-    setTimeout(function () {
-      respawn();
-      setTimeout(function () {
-        reviving = false;
-        smart_move(position, function () {
-          fighting = true;
-        });
-      }, 1000);
-    }, 12000);
+  //Switch statement decides what we should do based on the value of 'state'
+  switch (state) {
+    case "farm":
+      farm();
+      break;
+    case "resupply_potions":
+      resupply_potions();
+      break;
   }
+}, 100); //Execute 10 times per second
 
-  if (
-    (can_use("hp") && character.hp / character.max_hp <= 0.5) ||
-    character.max_hp - character.hp > 200
-  ) {
-    use("hp");
-  }
-
-  if (
-    (can_use("mp") && character.mp / character.max_mp <= 0.5) ||
-    character.max_mp - character.mp > 300
-  ) {
-    use("mp");
-  }
+//Potions And Looting
+setInterval(function () {
   loot();
 
-  let target;
-  target = get_nearest_monster({ type: targetMonster });
-  change_target(target);
-  if (target) {
-    if (can_attack(target)) {
-      attack(target);
-    } else {
-      let dist = Math.sqrt(
-        Math.pow(target.real_x - character.real_x, 2) +
-          Math.pow(target.real_y - character.real_y, 2)
-      );
-      if (dist > character.range - 20)
-        move(
-          (target.real_x + character.real_x) / 2,
-          (target.real_y + character.real_y) / 2
-        );
+  //Heal With Potions if we're below expected hp or mp.
+  if (
+    character.hp / character.max_hp < should_heal_hp ||
+    character.mp / character.max_mp < should_heal_mp
+  ) {
+    logCharacter("Using healing potion");
+    use_hp_or_mp();
+  }
+}, 500); //Execute 2 times per second
+
+function state_controller() {
+  //Default to farming
+  var new_state = "farm";
+
+  //Do we need potions?
+  for (type_id in potion_types) {
+    var type = potion_types[type_id];
+
+    var num_potions = num_items(type);
+
+    if (num_potions < min_potions) {
+      logCharacter("Setting new state to resupply potions");
+      new_state = "resupply_potions";
+      break;
     }
   }
-}, 1000 / 4);
+
+  if (state != new_state) {
+    state = new_state;
+  }
+}
+
+//This function contains our logic for when we're farming mobs
+function farm() {
+  var target = find_viable_targets()[0];
+  //Attack or move to target
+  if (target != null) {
+    if (distance_to_point(target.real_x, target.real_y) < character.range) {
+      if (can_attack(target)) {
+        attack(target);
+        logCharacter(
+          `Attacking id: ${target.id} name :${target.name} hp: ${target.hp}`
+        );
+      }
+    } else {
+      move_to_target(target);
+    }
+  } else {
+    if (!smart.moving) {
+      logCharacter("Finding a target");
+      smart_move({ to: monster_targets[0] });
+    }
+  }
+}
+
+//This function contains our logic during resupply runs
+function resupply_potions() {
+  var potion_merchant = get_npc("fancypots");
+
+  var distance_to_merchant = null;
+
+  if (potion_merchant != null) {
+    distance_to_merchant = distance_to_point(
+      potion_merchant.position[0],
+      potion_merchant.position[1]
+    );
+  }
+
+  if (
+    !smart.moving &&
+    (distance_to_merchant == null || distance_to_merchant > 250)
+  ) {
+    smart_move({ to: "potions" });
+  }
+
+  if (distance_to_merchant != null && distance_to_merchant < 250) {
+    buy_potions();
+  }
+}
+
+//Buys potions until the amount of each potion_type we defined in the start of the script is above the min_potions value.
+function buy_potions() {
+  if (empty_slots() > 0) {
+    for (type_id in potion_types) {
+      var type = potion_types[type_id];
+
+      var item_def = parent.G.items[type];
+
+      if (item_def != null) {
+        var cost = item_def.g * purchase_amount;
+
+        if (character.gold >= cost) {
+          var num_potions = num_items(type);
+
+          if (num_potions < min_potions) {
+            buy(type, purchase_amount);
+          }
+        } else {
+          logCharacter("Not Enough Gold!");
+        }
+      }
+    }
+  } else {
+    logCharacter("Inventory Full!");
+  }
+}
+
+//Returns the number of items in your inventory for a given item name;
+function num_items(name) {
+  var item_count = character.items
+    .filter((item) => item != null && item.name == name)
+    .reduce(function (a, b) {
+      return a + (b["q"] || 1);
+    }, 0);
+
+  return item_count;
+}
+
+//Returns how many inventory slots have not yet been filled.
+function empty_slots() {
+  return character.esize;
+}
+
+//Gets an NPC by name from the current map.
+function get_npc(name) {
+  var npc = parent.G.maps[character.map].npcs.filter((npc) => npc.id == name);
+
+  if (npc.length > 0) {
+    return npc[0];
+  }
+
+  return null;
+}
+
+//Returns the distance of the character to a point in the world.
+function distance_to_point(x, y) {
+  return Math.sqrt(
+    Math.pow(character.real_x - x, 2) + Math.pow(character.real_y - y, 2)
+  );
+}
+
+//This function will ether move straight towards the target entity,
+//or utilize smart_move to find their way there.
+function move_to_target(target) {
+  if (can_move_to(target.real_x, target.real_y)) {
+    smart.moving = false;
+    smart.searching = false;
+    move(
+      character.real_x + (target.real_x - character.real_x) / 2,
+      character.real_y + (target.real_y - character.real_y) / 2
+    );
+  } else {
+    if (!smart.moving) {
+      smart_move({ x: target.real_x, y: target.real_y });
+    }
+  }
+}
+
+//Returns an ordered array of all relevant targets as determined by the following:
+////1. The monsters' type is contained in the 'monsterTargets' array.
+////2. The monster is attacking you or a party member.
+////3. The monster is not targeting someone outside your party.
+//The order of the list is as follows:
+////Monsters attacking you or party members are ordered first.
+////Monsters are then ordered by distance.
+function find_viable_targets() {
+  var monsters = Object.values(parent.entities).filter(
+    (mob) =>
+      ((mob.target == null ||
+        parent.party_list.includes(mob.target) ||
+        mob.target == character.name) &&
+        mob.type == "monster" &&
+        (parent.party_list.includes(mob.target) ||
+          mob.target == character.name)) ||
+      monster_targets.includes(mob.mtype)
+  );
+
+  for (id in monsters) {
+    var monster = monsters[id];
+
+    if (
+      parent.party_list.includes(monster.target) ||
+      monster.target == character.name
+    ) {
+      monster.targeting_party = 1;
+    } else {
+      monster.targeting_party = 0;
+    }
+  }
+
+  //Order monsters by whether they're attacking us, then by distance.
+  monsters.sort(function (current, next) {
+    if (current.targeting_party > next.targeting_party) {
+      return -1;
+    }
+    var dist_current = distance(character, current);
+    var dist_next = distance(character, next);
+    // Else go to the 2nd item
+    if (dist_current < dist_next) {
+      return -1;
+    } else if (dist_current > dist_next) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  return monsters;
+}
