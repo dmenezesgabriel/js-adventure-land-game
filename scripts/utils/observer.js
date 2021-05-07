802bca86d50cf8a02bcb48bac89bbce360ffc7a5
Gabriel <dmenezes.gabriel@gmail.com>
2021-05-06 21:17:37 -0300

    Add actor and target names to combat logs

--- scripts/utils/observer.js
+++ scripts/utils/observer.js
@@ -2,7 +2,15 @@ class Observer {
   // Log character events
   constructor(obj) {
     obj.all(function (name, data) {
+      // Set event name
       data.event_name = name;
+
+      const combat_events = ["incoming", "hit", "target_hit"];
+      // Get entity name for each event
+      if (combat_events.includes(data.event_name)) {
+        data.target_name = get_entity(data.target).name;
+        data.actor_name = get_entity(data.actor).name;
+      }
       console.log(JSON.stringify(data));
     });
   }
