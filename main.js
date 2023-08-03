f94ac68f90f3745c4cfb10b3861845274e73dfeb
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-08-02 21:27:17 -0300

    Update main

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ game_log("Hello");
 // show_json(character);
 game_log(parent.character.hp);
 game_log(parent.character.x);
+game_log(get_nearest_monster({ max_att: 100, min_xp: 10, target: "Name", no_target: true }));
+// smart_move("goo");
+smart_move({ x: 0, y: 0, map: "main" });
