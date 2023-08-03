5a43c5d97042edd7a38516fc778e12d619930871
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-08-03 00:11:17 -0300

    Update main

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
-game_log("Hello");
-// show_json(character);
-game_log(parent.character.hp);
-game_log(parent.character.x);
-game_log(get_nearest_monster({ max_att: 100, min_xp: 10, target: "Name", no_target: true }));
-// smart_move("goo");
-smart_move({ x: 0, y: 0, map: "main" });
+function getTarget() {
+  let target = get_targeted_monster();
+  if (!target) {
+    target = get_nearest_monster();
+    if (target) change_target(target);
+    else {
+      set_message("No target available");
+      return;
+    }
+  }
+  return target;
+}
+
+function circleTarget(character, target) {
+  let radius = character.range - 50;
+  let theta = Math.atan2(character.y - target.y, character.x - target.x) + 180 / Math.PI;
+
+  draw_circle(target.x, target.y, radius, 1, 0x00ff00);
+  move(target.x + radius * Math.cos(theta), target.y + radius * Math.sin(theta));
+}
+
+function main() {
+  let attackMode = true;
+
+  game_log("Hello", "white");
+
+  setInterval(function () {
+    use_hp_or_mp();
+    loot();
+
+    if (!attackMode || character.rip || is_moving(character)) return;
+
+    let target = getTarget();
+    if (target) {
+      if (!is_in_range(target)) {
+        move(character.x + (target.x - character.x) / 2, character.y + (target.y - character.y) / 2);
+      } else if (can_attack(target)) {
+        attack(target);
+        circleTarget(character, target);
+      }
+    }
+
+    clear_drawings();
+  }, 1000 / 4);
+}
+
+main();
